b7f9dca61c7f289fa7d590a9fe820de20ba03fe3
hinal-025 <68282568+hinal-025@users.noreply.github.com>
2020-08-21 09:57:34 +0530

    Add files via upload

---
+++ C_loop.c
@@ -0,0 +1,20 @@
+/*C program draw something like stairs using looping statement*/
+#include<stdlib.h>
+#include<stdio.h>
+int main()
+{
+    system("cls");
+    int i,j,n;
+    printf("\nEnter number of stair you want : ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    system("pause");
+	return 10;
+}
